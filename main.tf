c47e21ab754558f797055fe2e43fd04788e0ab82
Justin Campbell <justin@justincampbell.me>
2019-06-14 09:49:39 -0400

    🍩

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "random_id" "random" {
+  byte_length = 8
+}
+
+output "shop_name" {
+  value = "Duck Donuts"
+}
+
+output "flavors" {
+  value = [
+    "Maple Bacon",
+    "Old-Fashioned",
+    "Glazed w/ Rainbow Sprinkles",
+    "Blueberry Glaze"
+  ]
+}
+
+output "orders" {
+  value = {
+    Justin = "Maple Bacon"
+    Piper = "Glazed w/ Rainbow Sprinkles"
+    Marissa = "Blueberry"
+    Beth = null
+  }
+}
